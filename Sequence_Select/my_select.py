1f45b58886cf60c2c787f47c56044a216d9d778a
Cryszzz <crystalzou000829@gmail.com>
2022-07-20 10:01:17 -0700

    finish

--- Sequence_Select/my_select.py
+++ Sequence_Select/my_select.py
@@ -25,10 +25,13 @@ print("\n")
 records = list(SeqIO.parse(SeqFile, "fasta"))
 sequence=[]
 for i in range(k):
-    c_seq=random.randint(0,len(records)-1)
-    seq_len=len(records[c_seq].seq)
-    start=random.randint(0,seq_len-l)
-    frag=records[c_seq].seq[start:(start+l)]
+    for j in range(5):
+        c_seq=random.randint(0,len(records)-1)
+        seq_len=len(records[c_seq].seq)
+        start=random.randint(0,seq_len-l)
+        frag=records[c_seq].seq[start:(start+l)]
+        if "a" not in frag or "N" not in frag or "t" not in frag or "g" not in frag or "c" not in frag:
+            break
     record = SeqRecord(frag, "fragment_%i" % (i + 1), "", "")
     sequence.append(record)
 
