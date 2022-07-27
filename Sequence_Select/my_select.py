159f9b1641a308faca9e3a3d52ae3d0b30500520
Cryszzz <crystalzou000829@gmail.com>
2022-07-26 18:30:15 -0700

    multiple_file

--- Sequence_Select/my_select.py
+++ Sequence_Select/my_select.py
@@ -11,10 +11,10 @@ path = "./../genomes"
 dir_list = os.listdir(path)
 
 # Arguments passed
-f=random.randrange(0,len(dir_list))
+
 k= random.randint(0,10) 
 l= random.randint(0,300) 
-SeqFile=path+"/"+dir_list[f]
+
 
 for i in range(1, len(sys.argv)):
     if sys.argv[i]=="-l":
@@ -26,10 +26,13 @@ print("Number of regions:", k)
 print("Region Length :", l)
 print("\n")
 
-records = list(SeqIO.parse(SeqFile, "fasta"))
+
 sequence=[]
 for i in range(k):
     for j in range(5):
+        f=random.randrange(0,len(dir_list))
+        SeqFile=path+"/"+dir_list[f]
+        records = list(SeqIO.parse(SeqFile, "fasta"))
         c_seq=random.randint(0,len(records)-1)
         seq_len=len(records[c_seq].seq)
         start=random.randint(0,seq_len-l)
