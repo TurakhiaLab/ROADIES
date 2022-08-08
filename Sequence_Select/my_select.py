84b47d0b3486c72048bb5dc830a135a68facdd51
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-08-08 19:08:39 +0000

    parallelized sequence select

--- Sequence_Select/my_select.py
+++ Sequence_Select/my_select.py
@@ -8,58 +8,49 @@ import numpy as np
 import os
 from alive_progress import alive_bar 
 # Get the list of all files and directories
-path = "./../genomes"
-
 # Arguments passed
-
 k= random.randint(0,10) 
 l= random.randint(0,300) 
-
-
 for i in range(1, len(sys.argv)):
     if sys.argv[i]=="-l":
         l=int(sys.argv[i+1])
     elif sys.argv[i]=="-k":
         k=int(sys.argv[i+1])
-    elif sys.argv[i]=="-d":
-        path = sys.argv[i+1]
-dir_list = os.listdir(path)
-
+path = sys.argv[1]
 print("Number of regions:", k)
 print("Region Length :", l)
 print("Input Directory:" +path)
 print("\n")
-print("Number of genomes:" +str(len(dir_list)))
-filenum = np.random.randint(0, len(dir_list), k)
-filenum.sort()
-
+records = list(SeqIO.parse(path,"fasta"))
 sequence=[]
+name = ''
+split = path.split('/')
+for s in split:
+    if 'fa' in s:
+        name = s
 with alive_bar(k) as bar:
     for i in range(k):
-        if i==0 or filenum[i]!=filenum[i-1]:
-            f=filenum[i]
-            SeqFile=path+"/"+dir_list[f]
-            #print(SeqFile)
-            records = list(SeqIO.parse(SeqFile, "fasta"))
         for j in range(5):
             c_seq=random.randint(0,len(records)-1)
             seq_len=len(records[c_seq].seq)
-            start=random.randint(0,seq_len-l)
-            frag=records[c_seq].seq[start:(start+l)]
+            start = random.randint(0,seq_len-l)
+            frag = records[c_seq].seq[start:(start+l)]
             if j ==4:
-                print("no good samples defaulting: "+frag)
-                record = SeqRecord(frag, "fragment_%i" % (i + 1), "", "")
-                sequence.append(record)
+                record = SeqRecord(frag, name+':'+str(start)+':'+str(i),"","" )
+                if "N" not in record:
+                    sequence.append(record)
                 break
             if 'a' not in frag and "N" not in frag and 't' not in frag and 'g' not in frag and 'c' not in frag:
-                print(frag)
-                record = SeqRecord(frag, "fragment_%i" % (i + 1), "", "")
+                record = SeqRecord(frag, name+str(i), "", "")
                 sequence.append(record)
                 break
             
-
         time.sleep(0.01)
         bar()
-
-SeqIO.write(sequence, "out.fasta", "fasta")
+frag = ''
+split = path.split('/')
+for s in split:
+    if 'fa' in s:
+        frag = s
+SeqIO.write(sequence, "frag_"+ s, "fasta")
 
--- lastz_align/align.sh
+++ lastz_align/align.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-mkdir alignments
+mkdir alignments2
 filepath=`pwd`
-for FILE in $1/*; do
-	echo "aligning ${FILE} to sequences"
-	/home/AD.UCSD.EDU/ttl074/lastz-distrib/bin/lastz_32 ${FILE}[multiple] ../Sequence_Select/out.fasta[multiple] --notransition --step=20  --format=maf --noytrim --filter=coverage:70 --filter=identity:70 >${FILE}.maf 
-done
+echo "$1" | parallel /home/AD.UCSD.EDU/ttl074/lastz-distrib/bin/lastz_32 [multiple] ../Sequence_Select/out.fasta[multiple] --notransition --step=20  --format=maf --noytrim --filter=coverage:70 --filter=identity:70 >${FILE}.maf 
 cd $1
-mv ./*.maf ${filepath}/alignments
+mv ./*.maf ${filepath}/alignments2
 cd ${filepath}
