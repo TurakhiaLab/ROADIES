ecf801fc808d72c79e6dc6b5f87da6440810f8f3
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-08-10 20:14:15 +0000

    editted get_fasta

--- lastz_align/align.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir alignments2
-filepath=`pwd`
-echo "$1" | parallel /home/AD.UCSD.EDU/ttl074/lastz-distrib/bin/lastz_32 [multiple] ../Sequence_Select/out.fasta[multiple] --notransition --step=20  --format=maf --noytrim --filter=coverage:70 --filter=identity:70 >${FILE}.maf 
-cd $1
-mv ./*.maf ${filepath}/alignments2
-cd ${filepath}
