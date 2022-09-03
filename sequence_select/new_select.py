65f36f2dd4691ec7e5e59b63b0b6091ef3b4664b
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-09-03 20:59:35 +0000

    new selection with threshold and start and end indices

--- sequence_select/new_select.py
+++ sequence_select/new_select.py
@@ -1,6 +1,5 @@
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
 import sys
 import time
 import random
@@ -8,60 +7,70 @@ import random
 import numpy as np
 import os
 from alive_progress import alive_bar 
-import re
+import argparse, os
 # Get the list of all files and directories
 # Arguments passed
-k= random.randint(0,10) 
-l= random.randint(0,300) 
-for i in range(1, len(sys.argv)):
-    if sys.argv[i]=="-l":
-        l=int(sys.argv[i+1])
-    elif sys.argv[i]=="-k":
-        k=int(sys.argv[i+1])
-path = sys.argv[1]
-""" print("Number of regions:", k)
-print("Region Length :", l)
-print("Input Directory:" +path)
-print("\n") """
-def CheckLength(letter):
-    return True if len(letter)>=l else False
-
-
+parser = argparse.ArgumentParser(description='genome selection')
+parser.add_argument('-l',type=int,default = 1000)
+parser.add_argument('-s',type=int,required=True)
+parser.add_argument('-e',type=int,required=True)
+parser.add_argument('-r',type=int,default=5)
+parser.add_argument('-t',type=float,default=0.7)
+parser.add_argument('--input',default="./../genomes")
+parser.add_argument('--output',default="./index.csv")
+args = parser.parse_args()
+s= args.s
+e= args.e
+l= args.l
+r = args.r
+t = args.t
+path = args.input
+output=args.output
+k = e-s+1
+if k < 1:
+    print("Invalid indices")
+    exit(1)
+print("Number of regions:", k)
+print("Region Length:", l)
+print("Input File:" +path)
 records = list(SeqIO.parse(path,"fasta"))
 sequence=[]
-name = ''
-split = path.split('/')
-for s in split:
-    if 'fa' in s:
-        name = s
+index = s
+threshold = int(t*l)
 with alive_bar(k) as bar:
     for i in range(k):
-        for j in range(5):
+        for j in range(r):
+            prev = ""
             c_seq=random.randint(0,len(records)-1)
             seq_len=len(records[c_seq].seq)
             if l>seq_len:
                 continue
-            new_list=list(filter(CheckLength,re.split("[a,c,g,t,N]", str(records[c_seq].seq))))
-            if len(new_list)==0:
-                continue
-            chosen=random.randint(0,len(new_list)-1)
-            start = random.randint(0,len(new_list[chosen])-l)
-            frag = Seq(new_list[chosen][start:(start+l)])
-            if j ==4:
-                record = SeqRecord(frag, name+':'+str(start)+':'+str(i),"","" )
-                if "N" not in record:
-                    sequence.append(record)
-                break
-            if 'a' not in frag and "N" not in frag and 't' not in frag and 'g' not in frag and 'c' not in frag:
-                record = SeqRecord(frag, name+str(i), "", "")
+            start = random.randint(0,seq_len-l)
+            frag = records[c_seq].seq[start:(start+l)]
+            upper = 0
+            for i in range(len(frag)):
+                if frag[i] =='N':
+                    break
+                if frag[i]=='A' or frag[i] =='T' or frag[i]=='G' or frag[i]=='T':
+                    upper = upper +1
+            if upper >= threshold:
+                record = SeqRecord(frag, "gene_"+str(index),"","")
                 sequence.append(record)
+                index = index+1
                 break
-            
+            else:
+                if j == r-1:
+                    record = SeqRecord(frag, "gene_"+str(index),"","" )
+                    if "N" not in record:
+                        sequence.append(record)
+                        index = index +1
+                    else:
+                        sequence.append(prev)
+                        index = index + 1
+                    break
+                prev = SeqRecord(frag, "gene_"+str(index),"","")
         time.sleep(0.01)
         bar()
-frag = ''
-split = path.split('/')
-for s in split:
-    if 'fa' in s:
-        frag = s
-SeqIO.write(sequence, "frag_"+ s, "fasta")
+
+SeqIO.write(sequence, output, "fasta")
+
