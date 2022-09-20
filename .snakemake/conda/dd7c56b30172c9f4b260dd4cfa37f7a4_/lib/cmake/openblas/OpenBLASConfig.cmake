a0d7401ebb420d628bceb17049913512801a51ce
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-09-20 19:53:19 +0000

    too much conda

--- .snakemake/conda/dd7c56b30172c9f4b260dd4cfa37f7a4_/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20")
-SET(OpenBLAS_INCLUDE_DIRS /home/AD.UCSD.EDU/ttl074/wga/wga-phylo/.snakemake/conda/dd7c56b30172c9f4b260dd4cfa37f7a4_/include)
-SET(OpenBLAS_LIBRARIES /home/AD.UCSD.EDU/ttl074/wga/wga-phylo/.snakemake/conda/dd7c56b30172c9f4b260dd4cfa37f7a4_/lib/libopenblas.so)
--- .snakemake/conda/dd7c56b30172c9f4b260dd4cfa37f7a4_/lib/python3.5/site-packages/reportlab/pdfbase/__init__.py
+++
@@ -1,6 +0,0 @@
-#Copyright ReportLab Europe Ltd. 2000-2017
-#see license.txt for license details
-#history https://bitbucket.org/rptlab/reportlab/history-node/tip/src/reportlab/pdfbase/__init__.py
-__version__='3.3.0'
-__doc__="""Internal functionality for creating PDF files - not part of API
-"""
