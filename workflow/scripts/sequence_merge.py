9be3611c4823860b878bcfd64400b07b653e3181
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-10-03 14:51:41 -0700

    sequence merge script with plot

---
+++ workflow/scripts/sequence_merge.py
@@ -0,0 +1,36 @@
+import shutil
+import glob,os,sys
+import seaborn as sns 
+import matplotlib.pyplot as plt
+directory = sys.argv[1]
+output = sys.argv[2]
+#stats = sys.argv[3]
+species_count = {}
+with open(output, 'wb') as outfile:
+    for filename in glob.glob(os.path.join(directory,'*.fa')):
+        if filename == output:
+            # don't want to copy the output into the output
+            continue
+        f = open(filename,'r')
+        t = f.read()
+        num = t.count('>')
+        s = filename.split('/')
+        print(s[len(s)-1])
+        n = s[len(s)-1].split('_')
+        name = n[0]+'_'+n[1]
+        species_count[name] = num
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
+print(species_count)
+#with open(stats,'w') as stat:
+ #   for species in species_count:
+  #      print(species,species_count[species])
+   #     stat.write(species+': '+str(species_count[species])+'\n')
+x = list(species_count.keys())
+y = list(species_count.values())
+ax = sns.barplot(x=x,y=y)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=40,ha='right')
+ax.set_title('Number of Reference Genes from Each Genome')
+plt.tight_layout()
+fig = ax.get_figure()
+fig.savefig("results/plots/sampling.png")
