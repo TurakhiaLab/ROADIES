6ab5cb2eb147a7b44d25f7f5e4a2db5cabd35cf6
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2022-10-11 14:00:50 -0700

    some runs with different parameters

--- workflow/scripts/get_run.sh
+++ workflow/scripts/get_run.sh
@@ -4,3 +4,4 @@ mkdir runs/$1/statistics
 cp -a results/plots runs/$1/plots
 cp -a results/statistics runs/$1/statistics
 cp results/speciesTree.newick runs/$1
+cp results.speciesTree.newick runs/trees 
