67232978f58ba8c6d8b46badc3019b6b857cacf2
ttl074 <ttl074@AD.UCSD.EDU@raptor.ucsd.edu>
2023-01-18 12:40:47 -0800

    fixed converge stats

--- workflow/scripts/c_stats.py
+++ workflow/scripts/c_stats.py
@@ -11,29 +11,33 @@ length = sys.argv[2]
 geneiter = sys.argv[3]
 #function to parse stat file for x and y values
 def get_run(statfile):
+    #dictionary for runs
     run_dict=OrderedDict()
+    #open statfiles
     with open(dir+'/'+statfile,'r') as f:
+        #read in lines
         lines=f.readlines()
+        #go through two lines at a time 
         for i in range(len(lines)):
             if i%2==0:
-                line = lines[i].strip()
-                #print(line)
-                cs = line.split(',')
+                #we will parse for the name of the second tree for each line to assign a mapping
+                cs = lines[i].strip().split(',')
                 cs2 = cs[1].split('/')
-                #print(cs2)
+                print(cs2)
                 idx = 0
+                #trying to parse for the run#
                 for j in range(len(cs2)):
                     if 'run' in cs2[j]:
                         idx = j
                 cs3 = cs2[idx].replace('.nwk','')
-                #print(cs3,idx)
+                print(cs3,idx)
                 cs3 = cs3.replace('run_','')
-                #print(cs3)
+                print(cs3)
                 id = int(cs3)*int(geneiter)
                 run_dict[id] = float(lines[i+1])
     return run_dict
 
-for f in ['refdist.txt','iterdist.txt','refdist2.txt','iterdist2.txt']:
+for f in ['refdist.txt','iterdist.txt']:
     run_dict = get_run(f)
     lengths = list(run_dict.keys())
     #print(order)
@@ -45,10 +49,8 @@ for f in ['refdist.txt','iterdist.txt','refdist2.txt','iterdist2.txt']:
         y.append(run_dict[run])
     plt.plot(x,y,label='length='+str(length))
     plt.xlabel('Number of Genes')
-    if '2' in f:
-        plt.ylabel('RF Distance')
-    else:
-        plt.ylabel('Normalized Distance')
+   
+    plt.ylabel('Normalized Distance')
     if 'ref' in f:
         plt.title('Distance to Reference')
     else:
--- workflow/scripts/c_stats.sh
+++ workflow/scripts/c_stats.sh
@@ -1,11 +1,13 @@
-#USAGE: ./c_stats.sh [convergence dir] [outputdir] [length] [ref newick]
+#USAGE: ./c_stats.sh [convergence dir] [outputdir] [length] [k] [ref newick]
 #make sure that directories do not end with '/'
+rm -r $2
 mkdir $2
 arr=()
-for d in ${1}/*.nwk ; do
+touch $2/refdist.txt
+for d in $1/*.nwk; do
     echo "$d"
-    Rscript --vanilla workflow/scripts/dist.R $5 ${d} $2/refdist.txt
-    Rscript --vanilla workflow/scripts/dist2.R $5 ${d} $2/refdist2.txt
+    echo "Rscript --vanilla workflow/scripts/dist.R ${5} ${d} $2/refdist.txt"
+    Rscript --vanilla workflow/scripts/dist.R ${5} ${d} $2/refdist.txt
     arr+=($d)
 done 
 echo ${arr[@]}
@@ -17,7 +19,5 @@ do
     ITER2=$(expr $i + 1)
     echo "comparing ${arr[$i]} to ${arr[${ITER2}]}" 
     Rscript --vanilla workflow/scripts/dist.R ${arr[${i}]} ${arr[${ITER2}]} $2/iterdist.txt
-    echo "alternative"
-    Rscript --vanilla workflow/scripts/dist2.R ${arr[${i}]} ${arr[${ITER2}]} $2/iterdist2.txt
 done
 python3 workflow/scripts/c_stats.py $2 $3 $4
