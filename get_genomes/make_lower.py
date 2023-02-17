650d75e400f3a82fcbca666cb4dc8ddfa5c201f3
Tian Liu <47438769+ttl074@users.noreply.github.com>
2023-02-17 13:24:37 -0800

    Delete get_genomes directory

--- get_genomes/make_lower.py
+++
@@ -1,8 +0,0 @@
-import sys
-with open(sys.argv[1],'r') as f:
-    with open(sys.argv[2],'w') as w:
-        lines = f.readline()
-        for l in lines:
-            l = l.lower()
-            w.write(l)
-        
