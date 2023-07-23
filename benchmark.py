bd9558b075c3e80ed53391b607442d120dd345db
ttl074 <ttl074@AD.UCSD.EDU@raptor-2.ucsd.edu>
2023-07-22 19:48:58 -0700

    fix benchmark code

--- benchmark.py
+++ benchmark.py
@@ -39,7 +39,7 @@ for i in range(len(steps)):
         print(top)
         tops.append(top)
     else:
-        tops.append(top)
+        tops.append(steps[i])
 with open(out_dir+"/step_avg.csv",'w') as w:
     for s in step_totals:
         w.write(s[0]+','+str(s[1])+','+str(s[2])+'\n')
